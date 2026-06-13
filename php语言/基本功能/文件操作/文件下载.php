7bbea0040ed181285c87824a36eab09c02a64240
whb54012 <1226844928@qq.com>
2026-06-13 14:22:12 +0800

    自动提交：更新PHP文件下载代码，新增文件修改与保存示例

--- php语言/基本功能/文件操作/文件下载.php
+++ php语言/基本功能/文件操作/文件下载.php
@@ -4,7 +4,18 @@ $d=opendir($dir);//打开目录
 $file=readdir($d);//按行读取目录结果
 is_dir($file);//判断是否为文件夹
 while($file=readdir($d)!==false){
+    if($file=='.'&&$file='..')
+        continue;//把当前目录和上级目录所表示的点跳过打印
     echo $file;
     //自动打印打开目录下的每一给目录或文件名
 }
+filetype($d);//0判断输入内容是文件还是目录
+unlink('相对路径/文件');//删除文件,不能删除文件夹
+
+// 文件下载设置http头格式控制
+header('Content-Type: application/octet-stream');//声明二进制类型,用于兼容老浏览器
+header('Content-Disposition: attachment; filename="自定义文件名"');//弹出下载框
+header('Content-Length: ' . filesize('相对路径/文件'));//将文件大小告诉浏览器,显示下载进度
+
+readfile('相对路径/文件')//打开文件,配合下载框头部设置可变成下载文件
 ?>
---
+++ php语言/基本功能/文件操作/文件修改与保存.php
@@ -0,0 +1,25 @@
+<?php
+$file=file_get_contents('文件路径');//一次读取全部文件内容
+echo "<form method='post'>
+    <textarea name='txt' rows='15' cols='80'><?=$file?></textarea>
+    <br>
+    <button type='submit'>保存修改</button>
+</form>";
+//将文件内容打印到textarea内部进行修改并上传
+$txt=$_POST['txt'];
+file_put_contents($file,$txt);
+//file_put_contents(文件路径,修改内容);将旧内容覆盖到新内容里面完成修改文件
+
+// 直接写入
+$content='内容';
+$f=fopen($file,'w');
+//fopen(文件路径,'操作模式')
+// r           r+          w           w+          a           a+
+// 读取        读写        写入        读写        追加写入      追加读写
+// 读取内容    写入内容    清空内容    可读可写    在尾部写入     可读可写
+//            一个个覆盖   写入,没有
+//            前面内容     就新建个
+fgetc($f);//读取打开的文件,只适用于有读取功能的
+fwrite($f,$content);
+//fwrite(打开的文件,写入内容)
+?>
