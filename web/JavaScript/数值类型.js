7c20aa2498bc334aa2d1c1b1d66db17bdf7126dd
whb54012 <1226844928@qq.com>
2026-06-15 09:53:34 +0000

    自动提交：更新 ${2026/6/15 17:53ilenPM53e}

--- web/JavaScript/数值类型.js
+++ web/JavaScript/数值类型.js
@@ -1,7 +1,8 @@
-
-        let arr1=[1,2,3];
-        let arr2=Array(1,2,3);
-        let arr3=new Array(1,2,3);
+var a=1;
+var b='abc';
+let arr1=[1,2,3];
+let arr2=Array(1,2,3);
+let arr3=new Array(1,2,3);
 //若内部元素只有一个，就用第一个，第二格和第三个有歧义，会将它输入的元素默认为此数组长度
 //如let arr1=Array[1]会将它理解为长度为一的数组;
 document.write(arr1);
