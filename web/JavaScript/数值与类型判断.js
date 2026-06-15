dfdb7b1cbea6a3146d19326fbb02508131334f3a
whb54012 <1226844928@qq.com>
2026-06-15 11:45:04 +0000

    自动提交：更新 ${2026/6/15 19:45ilenPM45e}

--- web/JavaScript/数值与类型判断.js
+++ web/JavaScript/数值与类型判断.js
@@ -2,7 +2,7 @@ var a=1;
 var b=1;
 var c="1";
 var d="2";
-document.write(a==b);
-document.write(a==c);//双等号时比较内容，无需在意类型
-document.write(a===c);//三等号时会比较类型和内容两者
-document.write(a!==c);//不全等或不全等
+console.log(a==b);
+console.log(a==c);//双等号时比较内容，无需在意类型
+console.log(a===c);//三等号时会比较类型和内容两者
+console.log(a!==c);//不全等或不全等
