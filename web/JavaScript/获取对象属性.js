3259e036a634f0bfb4e57b68d764c7f01808a327
whb54012 <1226844928@qq.com>
2026-06-17 09:42:23 +0000

    自动提交：更新 ${2026/6/17 17:42ilenPM42e}

--- web/JavaScript/获取对象属性.js
+++ web/JavaScript/获取对象属性.js
@@ -1,3 +1,18 @@
 let a=document.getElementsByClassName('.class属性名');
 let b=document.getElementById('#id属性名');
 let c=document.querySelector('标签/.属性/#id名');
+function update(){
+    let s=document.querySelector('img');
+    console.log(s.src);
+    // 获取标签属性
+}
+function update1(){
+    let s=document.querySelector('h1');
+    const a=s.id;
+  // 获取截取对象的指定属性
+    s.innerText="文本内容<br>"//不会被渲染,原样输出
+    s.innerHTML="文本内容<br>"//会被渲染
+// innerhtml和innertext区别在于会不会被html渲染
+    console.log(s.innerHTML);
+    // 获取内部文字 + 内嵌 HTML 标签
+}
