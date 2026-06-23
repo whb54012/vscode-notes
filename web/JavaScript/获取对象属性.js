b3d65a4335308e7c09854706cc22b3142a05d7cc
whb54012 <1226844928@qq.com>
2026-06-23 07:22:20 +0000

    自动提交：更新 ${2026/6/23 15:22ilenPM22e}

--- web/JavaScript/获取对象属性.js
+++ web/JavaScript/获取对象属性.js
@@ -1,5 +1,5 @@
 let a=document.getElementsByClassName('class属性名');
-let b=document.getElementById('#id属性名');
+let b=document.getElementById('id属性名');
 let c=document.querySelector('标签/.属性/#id名');
 function update(){
     let s=document.querySelector('img');
