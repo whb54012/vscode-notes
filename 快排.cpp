0582345bac19a091474b16b9d51e12cfed120993
whb54012 <1226844928@qq.com>
2026-06-23 15:41:32 +0000

    自动提交：更新 ${2026/6/23 23:41ilenPM41e}

--- 快排.cpp
+++ 快排.cpp
@@ -9,6 +9,9 @@ void quicksort(int arr[],int size,int i){
             arr[j]=arr[i];
             arr[i]=tmp;
             i++;}}
+            for(int m=0;m<=6;m++){
+        cout<<arr[m]<<endl;
+    }
     for(int m=j-1;m>=i;m--){
         int tmp=arr[m];
         arr[m]=arr[j];
