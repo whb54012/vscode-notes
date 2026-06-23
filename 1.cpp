981afc39929df79d0bfeeb2156484cf36fe45f52
whb54012 <1226844928@qq.com>
2026-06-23 06:03:22 +0000

    自动提交：更新 ${2026/6/23 14:03ilenPM3e}

--- 1.cpp
+++ 1.cpp
@@ -29,25 +29,25 @@ void max(int l,int r,int m,int arr[]){
     }
 }
 void summax(int arr[],int l,int r){
-    if(l==r){
+    if(l==r)//{
         if(arr[l]>MIN){
             MIN=arr[l];
             start=l;
-            End=r;
-        }
-        return;
-    }
+            End=r;}
+        // return arr[l];}
     int m=(l+r)/2;
     summax(arr,l,m);
     summax(arr,m+1,r);
     max(l,r,m,arr);
+    // return MAX(LR,MAX(L,R));
 }
 int main(){
     int arr[]={1,-3,2,3,4,-1,-3,6,8,10};
-    int left=0,right=9;
+    int l=0,right=9;
     MIN=-1000000;
     int total=0;
-    summax(arr,left,right);
+    summax(arr,l,right);
+    // int max=
     for(int i=start;i<=End;i++){
         total+=arr[i];
     }
