504f982d53c36ee3c425158bd5138acc0ab2bf41
whb54012 <1226844928@qq.com>
2026-06-23 14:51:07 +0000

    自动提交：更新 ${2026/6/23 22:51ilenPM51e}

--- 快排.cpp
+++ 快排.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 void quicksort(int arr[],int size){
     int i=0,j=0;
-    for
+    for{;j<=size;j++}{
+        if(arr[j]<arr[size]){
+            int tmp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=tmp;
+            i++;
+        }
+
+    }
 }
 int main(){
     int arr[]={1,9,1,2,1,0,1};
