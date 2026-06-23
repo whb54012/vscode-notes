8ac6d16041c7bab874886c61ec6fc561d5355475
whb54012 <1226844928@qq.com>
2026-06-23 03:55:48 +0000

    自动提交：更新 ${2026/6/23 11:55ilenAM55e}

--- 1.cpp
+++ 1.cpp
@@ -1,19 +1,27 @@
 #include<iostream>
 using namespace std;
-int sum(int l,int r,int m){
-    for(int i=m,i>=l,i--){
-
+int MAX(int max,int sum){
+    if(max<sum) return sum;
+    else return max;
+}
+int max(int l,int r,int m,int arr[]){
+    int suml=0,sumr=0,maxl=arr[m],maxr=arr[m+1];
+    for(int i=m;i>=l;i--){
+        suml+=arr[i];
+        maxl=MAX(maxl,suml);
     }
-    for(int i=m+1,i<=r,i++){
-
+    for(int i=m+1;i<=r;i++){
+        sumr+=arr[i];
+        maxr=MAX(maxl,suml);
     }
+    return maxr+maxl;
 }
 int summax(int arr[],int l,int r){
     if(l==r) return arr[l];
     int m=(l+r)/2;
     int L=summax(arr,l,m);
     int R=summax(arr,m+1,r);
-    int LR=max(l,r,m);
+    int LR=max(l,r,m,arr);
 }
 int main(){
     int arr[]={1,-3,2,3,4,-1,-3,6,8,10};
