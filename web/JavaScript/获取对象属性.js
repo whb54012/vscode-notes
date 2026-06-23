545b691cc40e6f178ab5ac66c10be3607f4978ea
whb54012 <1226844928@qq.com>
2026-06-23 07:20:59 +0000

    自动提交：更新 ${2026/6/23 15:20ilenPM20e}

--- web/JavaScript/获取对象属性.js
+++ web/JavaScript/获取对象属性.js
@@ -1,4 +1,4 @@
-let a=document.getElementsByClassName('.class属性名');
+let a=document.getElementsByClassName('class属性名');
 let b=document.getElementById('#id属性名');
 let c=document.querySelector('标签/.属性/#id名');
 function update(){
