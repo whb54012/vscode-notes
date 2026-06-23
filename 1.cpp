d28c79e1bfd546d3f342bac2adc2d05a4b44e96a
whb54012 <1226844928@qq.com>
2026-06-23 03:25:44 +0000

    自动提交：更新 ${2026/6/23 11:25ilenAM25e}

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
-int summax(int *)
+int summax(int arr[],int l,int r){
+
+}
 int main(){
     int arr[]={1,-3,2,3,4,-1,-3,6,8,10};
-    return summax(arr);
+    int,l,r;
+    return summax(arr,l,r);
 }
