43133794e3f699457c96d0a9afb893fa630dfe58
whb54012 <1226844928@qq.com>
2026-06-23 14:44:59 +0000

    自动提交：更新 ${2026/6/23 22:44ilenPM44e}

---
+++ 快排.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void quicksort(int arr[],int size){
+    int i=0,j=0;
+    for
+}
+int main(){
+    int arr[]={1,9,1,2,1,0,1};
+    int k;
+    cout<<k<<endl;
+    quicksort(arr,sizeof(arr)/4);
+}
