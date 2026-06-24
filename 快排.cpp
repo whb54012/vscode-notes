0038ac46d63ebe0441195158a83be1ce115d2730
whb54012 <1226844928@qq.com>
2026-06-24 05:51:31 +0000

    自动提交：更新 ${2026/6/24 13:51ilenPM51e}

--- 快排.cpp
+++ 快排.cpp
@@ -1,35 +1,36 @@
 #include<iostream>
 using namespace std;
-void quicksort(int arr[],int size,int i){
-    int j=i;
-    if(j>=size){return;}
-    for(;j<=size;j++){
-        if(arr[j]<arr[size]){
+void quicksort(int arr[],int r,int l){
+    int j=l,i=l;
+    if(j>=r){return;}
+    for(;j<=r;j++){
+        if(arr[j]<arr[r]){
             int tmp=arr[j];
             arr[j]=arr[i];
             arr[i]=tmp;
             i++;}}
-            for(int m=0;m<=6;m++){
-        cout<<arr[m];
-    }j--;
+    //         for(int m=0;m<=6;m++){
+    //     cout<<arr[m];
+    // }
+    j--;
     for(int m=j-1;m>=i;m--){
         int tmp=arr[m];
         arr[m]=arr[j];
         arr[j]=tmp;
         j--;
     }
-    cout<<j<<endl;
-    for(int m=0;m<=6;m++){
-        cout<<arr[m];
-    }
-    quicksort(arr,j-1,0);
-    quicksort(arr,size,j+1);
+    // cout<<j<<endl;
+    // for(int m=0;m<=6;m++){
+    //     cout<<arr[m];
+    // }
+    quicksort(arr,j-1,l);
+    quicksort(arr,r,j+1);
 }
 int main(){
-    int arr[]={1,9,1,2,1,0,1};
+    int arr[]={1,9,1,2,8,0,1};
     int k;
     quicksort(arr,sizeof(arr)/4-1,0);
-    // for(int m=0;m<=6;m++){
-    //     cout<<arr[m]<<endl;
-    // }
+    for(int m=0;m<=6;m++){
+        cout<<arr[m]<<endl;
+    }
 }
