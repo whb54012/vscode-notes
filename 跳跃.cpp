d4f46a03790b29c737d4c49c7be1e6864612f096
whb54012 <1226844928@qq.com>
2026-06-30 12:28:48 +0000

    自动提交：更新 ${2026/6/30 20:28ilenPM28e}

--- 跳跃.cpp
+++ 跳跃.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
+using namespace std;
     int function(int arr[],int i,int len,int se){
     if(i+len==i) return 0;
-    int Max=0;
+    int Max=0,m=1;
     int j=i;
     for(;j<len;j++){
     if(i+len>=se) return 1;
 	if(len+i>=se) return 1;
-    int k=Max+i+arr[i];
-	k++;
+    int k=Max+arr[j]+m;
+    m++;
     if(k>=Max){Max=k;i=j;}
     }
     return function(arr,i,arr[i],se);
@@ -15,7 +16,8 @@
       
    int main(){
          int arr[]={1,2,3,4,5,6};
-         int se=arr.size();
-         int function(arr,0,arr[0],se-1);
-    
+         int se = sizeof(arr) / sizeof(arr[0]);
+         int result = function(arr, 0, arr[0], se - 1);
+         std::cout << result;
+         return 0;
    }
