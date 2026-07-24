b6067aa76648b286ec51bdac57da457fae860a94
whb54012 <1226844928@qq.com>
2026-07-24 01:26:11 +0000

    自动提交：更新 ${2026/7/24 09:26ilenAM26e}

--- php语言/基本功能/目录查找与文件查看.php
+++ php语言/基本功能/目录查找与文件查看.php
@@ -1,6 +1,6 @@
 目录文件查找
 glob("*"); 正则匹配,通过通配符来查找当前目录符合条件的文件,可通过../来逃逸
-$变量=opendir("")
+$变量=opendir(".")用变量接收当前目录的所有文件
 whilereaddir()+closedir()
 highlight_file("相对/绝对文件名路径");
 readfile("相对/绝对文件名路径")
