fa1836e800ec3d87fd39ca77f0fe49e5130eb05b
whb54012 <1226844928@qq.com>
2026-07-26 04:59:20 +0000

    自动提交：更新 ${2026/7/26 12:59ilenPM59e}

--- 漏洞类型/sql注入/load_file和带外注入.sql
+++ 漏洞类型/sql注入/load_file和带外注入.sql
@@ -1,8 +1,8 @@
 root权限才可使用load_file等高危函数
-secure_file_priv权限为null时load_file才可读取任意路径
+secure_file_priv权限为空值时load_file才可读取任意路径
 load_file('绝对路径');
 高频语句,读取路径内文件的内容并返回
-hex(load_file('绝对路径');)
+hex(database())
 手动将内容转换成十六进制编码,以防出现不可控问题
 select * from user where id='参数';
 所有为参数的位置都可写十六进制由sql自动转义写入(参数位就是用字符串或数值类型写入的位置)
