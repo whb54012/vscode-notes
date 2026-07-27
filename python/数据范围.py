9cdfe41ef14b5ec94aade2ec25c92c9771390ace
whb54012 <1226844928@qq.com>
2026-07-27 06:17:06 +0000

    自动提交：更新 ${2026/7/27 14:17ilenPM17e}

--- python/数据范围.py
+++ python/数据范围.py
@@ -2,8 +2,8 @@ num=10#外界全局变量,已创建
 def one(a,b):
     global num,sum#变量声明,不创建新变量,使用外界相同变量
 # 如果外界不存在就以当前为全局变量并创建
-    sum=a+b
-    num=1
+    sum=a+b#创建全局变量sum
+    num=1#使用外界已有全局变量num并修改
     return
 one(1,2)
 print(num)
