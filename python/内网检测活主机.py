6bb8a02cece0353a281d98f4005f545020fbd133
whb54012 <1226844928@qq.com>
2026-07-27 11:21:03 +0000

    自动提交：更新 ${2026/7/27 19:21ilenPM21e}

--- python/内网检测活主机.py
+++ python/内网检测活主机.py
@@ -1,9 +1,9 @@
 import requests#激活模板
 for i in range(1,256):
-    ip = f"172.26.48.{i}"
+    ip = f"?c={i}"
     url = f"http://localhost:3000/%E7%BD%91%E9%A1%B5/%E4%B8%BB%E9%A1%B5/%E9%9F%B3%E4%B9%90.php{ip}"
     try:
         requests.get(url,timeout=2)#设置检测参数和超时时间
-        print(ip)
+        print(url)
     except:
         pass
