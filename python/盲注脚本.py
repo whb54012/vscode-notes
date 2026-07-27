50ce192a340dc4a01e43bbc5c73f5055babbe161
whb54012 <1226844928@qq.com>
2026-07-27 10:16:13 +0000

    自动提交：更新 ${2026/7/27 18:16ilenPM16e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -3,27 +3,16 @@ import queue
 import threading
 import warnings
 warnings.filterwarnings("ignore")
-num=int(input())
+result=int(input())
 end=int(input())
 url = input()
 data=input()
-def fun(url,result1):
-    for i in range(num):
-        try:
-            num1=result1.get(Timeout=0.5)
-            reponse=requests.get(url.format(num)+data.format(num1))
-            if '' in reponse.text:
-                print(num,url.format(num)+data.format(num1),chr(num1))
-        except:
+def fun(url,result,result2):
+    for i in result2:
+        reponse=requests.get(url+data.format(result,result2))
+        if '' in reponse.text:
+            print(result,url+format(result,result2),chr(result2))
             break
-        
-
-
-
-result1=queue.Queue()
-for i in range(1,end):
-    result1.put(i)
-    result1.empty
-for  i in range(1,26):
-    t=threading.Thread(target=fun,args=(url,result1))
+for  i in range(1,end+1):
+    t=threading.Thread(target=fun,args=(url,i,result))
     t.start()
