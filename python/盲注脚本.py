0ea42ac5c4d94726125ef7b64a727116df575aa1
whb54012 <1226844928@qq.com>
2026-07-27 09:40:08 +0000

    自动提交：更新 ${2026/7/27 17:40ilenPM40e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -13,13 +13,15 @@ def fun(url,result1):
         reponse=requests.get(url.format(num)+data.format(num1))
         if '' in reponse.text:
             print(num,url.format(num)+data.format(num1),chr(num1))
-        if result1.em:
+        if result1.empty():
+            break
 
 
 
 result1=queue.Queue()
 for i in range(1,end):
     result1.put(i)
+    result1.empty
 for  i in range(1,26):
     t=threading.Thread(target=fun,args=(url,result1))
     t.start()
