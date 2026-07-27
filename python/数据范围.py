1a5cf78667a88c6fee76bcca450cf3124b5a165e
whb54012 <1226844928@qq.com>
2026-07-27 06:11:53 +0000

    自动提交：更新 ${2026/7/27 14:11ilenPM11e}

--- python/数据范围.py
+++ python/数据范围.py
@@ -1,7 +1,9 @@
 num=10
 def one(a,b):
-    global num#变量声明,与外部全局变量指向同一个,如果外部不存在就以当前未全局变量
-    num=a+b
+    global num,sum#变量声明,与外部全局变量指向同一个
+# ,如果外部不存在就以当前未全局变量
+    sum=a+b
+    num=1
     return
 one(1,2)
 print(num)
