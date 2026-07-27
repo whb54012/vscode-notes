61534fd499dfe3824d80c183a78d896da8b716f3
whb54012 <1226844928@qq.com>
2026-07-27 11:16:34 +0000

    自动提交：更新 ${2026/7/27 19:16ilenPM16e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -3,7 +3,8 @@ import threading
 import warnings
 warnings.filterwarnings("ignore")
 result=int(input("输入查找长度"))
-url = input("输入url及注入点")
+# url = input("输入url及注入点")
+url=http://localhost:3000/%E7%BD%91%E9%A1%B5/%E4%B8%BB%E9%A1%B5/%E9%9F%B3%E4%B9%90.php
 data=input("输入注入语句")
 def fun(url,result,result2):
     for i in range(result2):
