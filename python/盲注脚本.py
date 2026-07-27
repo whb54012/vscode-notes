d353214a48ec98d2f362ac0d49da6b690bda39b1
whb54012 <1226844928@qq.com>
2026-07-27 11:51:41 +0000

    自动提交：更新 ${2026/7/27 19:51ilenPM51e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -4,7 +4,7 @@ import warnings
 import time
 warnings.filterwarnings("ignore")
 # result=int(input("输入查找长度"))
-result=4
+result=11
 # url = input("输入url及注入点")
 url="http://localhost:3000/%E7%BD%91%E9%A1%B5/%E4%B8%BB%E9%A1%B5/%E9%9F%B3%E4%B9%90.php"
 # data=input("输入注入语句")
@@ -14,8 +14,7 @@ def fun(url,result,result2):
         # try:
             reponse=requests.get(url+data.format(result,i),timeout=0.5)
             if 'login.php' in reponse.text:
-                time.sleep(3)
-                print(result+"\n")
+                print(result,"          ")
                 break
         # except:
         #     print("error\n")
