85b1f5ea46ff02a5e4d13b803fe4d5f798455fae
whb54012 <1226844928@qq.com>
2026-07-29 07:43:57 +0000

    自动提交：更新 ${2026/7/29 15:43ilenPM43e}

--- 1.py
+++ 1.py
@@ -2,8 +2,11 @@ import requests
 url=input()
 cookie=input()
 param=input()
+str='-0123456789abcdefghijklmnopqrstuvwxyz{|}'
+flag=""
 data={
-    
+
 }
 t=requests.get(url,data=data,params=param,cookies=cookie,timeout=1).text
-t.find()
+if t.find("")>t.find(""):
+    flag=flag+
