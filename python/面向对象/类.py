633b839659499334e2e46830c3253f45f30bbb10
whb54012 <1226844928@qq.com>
2026-07-29 05:01:42 +0000

    自动提交：更新 ${2026/7/29 13:01ilenPM1e}

--- python/面向对象/类.py
+++ python/面向对象/类.py
@@ -1,10 +1,11 @@
 class Car:#类名首字母必须大写
-    price=1000
+    price=1000#静态添加属性,可修改
 # 定义类
-
 对象名=Car()
-Car.name="bwm"#可动态添加属性
+Car.name="bwm"#可动态添加修改属性
 Car.color='red'
-
+Car.price=10
+print(Car.price)
 print(Car.__dict__)
 # 对象的内置函数,将对象属性按照字典格式打印
+#                   类方法
