ac3e1f460e8a579f25774a60ff3ebb42133e61c9
whb54012 <1226844928@qq.com>
2026-07-29 03:27:40 +0000

    自动提交：更新 ${2026/7/29 11:27ilenAM27e}

--- python/模块.py
+++ python/模块.py
@@ -16,3 +16,14 @@ if __name__=='__main__':
         print("函数被创建")
 """注意,python用条件或循环创建出来的函数或变量不会因为被执行完毕后就销毁,会一直
 保留到主程序结束"""
+
+# 自定义导入功能,使用from 模块名 import *时可以用__all__来限制导入部分
+__all__ = ['public_func', 'PublicClass','b']  # 声明公开接口
+def public_func():
+    print("我是公开函数")
+class PublicClass:
+    pass
+a=1
+b=2
+def _private_func():
+    print("我是私有函数（下划线开头）")
