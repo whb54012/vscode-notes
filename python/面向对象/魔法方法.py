8e2f8493a24d871c601f7f1e88f801d6e5435b27
whb54012 <1226844928@qq.com>
2026-07-30 02:13:26 +0000

    自动提交：更新 ${2026/7/30 10:13ilenAM13e}

--- python/面向对象/魔法方法.py
+++ python/面向对象/魔法方法.py
@@ -7,4 +7,6 @@ class Fun:
         self.name=name
     def __str__(self):
         return f"姓名：{self.name}，年龄：{self.age}"
-    person=Fun('whb',18)
+    # 返回字符串模式
+person=Fun('whb',18)
+print(person)#打印返回的字符串
