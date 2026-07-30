1f0ecb5032f590f0e766ece7638aeff47fb1af07
whb54012 <1226844928@qq.com>
2026-07-30 02:11:46 +0000

    自动提交：更新 ${2026/7/30 10:11ilenAM11e}

--- python/面向对象/魔法方法.py
+++ python/面向对象/魔法方法.py
@@ -1,2 +1,9 @@
 # python类自动调用的方法
 # __init__初始方法
+# __str__转换成字符串
+class fun:
+    def __init__(self,name,age):
+        self.age=age
+        self.name=name
+    def __str__(self):
+        return f"姓名：{self.name}，年龄：{self.age}"
