824bc44a42c72026745932bbed100517dbd9fb0b
whb54012 <1226844928@qq.com>
2026-08-04 04:26:52 +0000

    自动提交：更新 ${2026/8/4 12:26ilenPM26e}

--- python/爬虫/网页解析.py
+++ python/爬虫/网页解析.py
@@ -7,7 +7,7 @@ td_list=document.xpath("//div/tr/td/text()")
 print(td_list)
 # document.xpath("//标签/子标签/子标签/text()")
 # 子标签写得越多抓取的越准确,最后一个不写标签而是目标格式
-th_list=document.xpath("//div/tr[1]/td/text()")
+th_list=document.xpath("//div[1]/tr/td/text()")
 print(th_list)
 # 相同子标签太多时可用列表方法指定读取哪行子标签
 t_list=document.xpath("//div/tr/text()")
