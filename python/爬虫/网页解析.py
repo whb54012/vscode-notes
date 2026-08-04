169d78c70dd8989494c3f99aeafc69c4ee5970e9
whb54012 <1226844928@qq.com>
2026-08-04 03:50:00 +0000

    自动提交：更新 ${2026/8/4 11:50ilenAM50e}

---
+++ python/爬虫/网页解析.py
@@ -0,0 +1,10 @@
+import lxml.html
+with open("./读取网页.html")
+document=lxml.html.fromstring(reponse)
+# 将html格式转换成一类对象
+document.xpath("//标签/子标签/子标签/text()")
+# 子标签写得越多抓取的越准确,最后一个不写标签而是目标格式
+document.xpath("//标签/子标签/子标签[1]/text()")
+# 相同子标签太多时可用列表方法指定读取哪行子标签
+# 想将每行子标签单独打印不放在一起,就读取上级子标签,然后将每个上级子标签打印
+# 来进行分类
