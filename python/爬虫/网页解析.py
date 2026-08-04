0815f45612db6f3713de2d5a21b00c4e06b594ec
whb54012 <1226844928@qq.com>
2026-08-04 04:54:57 +0000

    自动提交：更新 ${2026/8/4 12:54ilenPM54e}

--- python/爬虫/网页解析.py
+++ python/爬虫/网页解析.py
@@ -4,6 +4,7 @@ with open("python\\爬虫\\读取网页.html","r",encoding="UTF-8") as f:
 document=lxml.html.fromstring(reponse)
 # 将html格式转换成一类对象
 td_list=document.xpath("//div/tr/td/text()")
+# x.path把对象接收成列表
 print(td_list)
 # document.xpath("//标签/子标签/子标签/text()")
 # 子标签写得越多抓取的越准确,最后一个不写标签而是目标格式
@@ -15,4 +16,4 @@ print(t_list)
 # 想将每行子标签单独打印不放在一起,就读取上级子标签,然后将每个上级子标签打印
 # 来进行分类
 for i in t_list:
-    pirnt(i/td)
+    td=i.xpath()
