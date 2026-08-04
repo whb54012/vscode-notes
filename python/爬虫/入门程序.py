5f3e3467773e2c86ebd6071f986509fb666714d6
whb54012 <1226844928@qq.com>
2026-08-04 03:41:43 +0000

    自动提交：更新 ${2026/8/4 11:41ilenAM41e}

--- python/爬虫/入门程序.py
+++ python/爬虫/入门程序.py
@@ -9,4 +9,4 @@ document=lxml.html.fromstring(reponse)
 document.xpath("//标签/子标签/子标签/text()")
 # 子标签写得越多抓取的越准确,最后一个不写标签而是目标格式
 document.xpath("//标签/子标签/子标签[1]/text()")
-# 相同子标签太多时可指定读取哪一行
+# 相同子标签太多时可用列表方法指定读取哪行子标签
