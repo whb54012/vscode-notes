62168421a24304f9a1aa6e9052da068f86b65948
whb54012 <1226844928@qq.com>
2026-08-04 03:34:26 +0000

    自动提交：更新 ${2026/8/4 11:34ilenAM34e}

--- python/爬虫/入门程序.py
+++ python/爬虫/入门程序.py
@@ -6,4 +6,5 @@ reponse=requests.get(url=url).text
 # 读取对应网站源码并转换为text格式
 document=lxml.html.fromstring(reponse)
 # 将html格式转换成一类对象
-print(document)
+document.xpath("//标签/子标签/子标签/text()")
+# 子标签写得越多抓取的越准确,最后一个不写标签而是目标格式
