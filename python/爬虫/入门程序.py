8797018dc6bd11bc523b5456ea39de743372a4cd
whb54012 <1226844928@qq.com>
2026-08-04 03:11:47 +0000

    自动提交：更新 ${2026/8/4 11:11ilenAM11e}

--- python/爬虫/入门程序.py
+++ python/爬虫/入门程序.py
@@ -1,7 +1,9 @@
 import requests
+import lxml
 url=input("url:")
 # 定义url
-reponse=requests.get(url=url)
-# 读取对应网站源码
-print(reponse.text)
+reponse=requests.get(url=url).text
+# 读取对应网站源码并转换为text格式
+print(reponse)
 # 读取网站源码为text格式
+
