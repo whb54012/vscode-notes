67d8cf1bf01c9f78ed3c9c37040a4debd5c132dc
whb54012 <1226844928@qq.com>
2026-08-04 03:40:25 +0000

    自动提交：更新 ${2026/8/4 11:40ilenAM40e}

--- python/爬虫/入门程序.py
+++ python/爬虫/入门程序.py
@@ -8,3 +8,5 @@ document=lxml.html.fromstring(reponse)
 # 将html格式转换成一类对象
 document.xpath("//标签/子标签/子标签/text()")
 # 子标签写得越多抓取的越准确,最后一个不写标签而是目标格式
+document.xpath("//标签/子标签/子标签[1]/text()")
+# 相同子标签太多时可指定读取哪一行
