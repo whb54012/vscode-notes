1013609e48662d04702468f728b0910a6d6234ff
whb54012 <1226844928@qq.com>
2026-08-04 03:27:52 +0000

    自动提交：更新 ${2026/8/4 11:27ilenAM27e}

--- python/爬虫/入门程序.py
+++ python/爬虫/入门程序.py
@@ -4,6 +4,6 @@ url=input("url:")
 # 定义url
 reponse=requests.get(url=url).text
 # 读取对应网站源码并转换为text格式
-document=lxml.html.fromastring(reponse)
+document=lxml.html.fromstring(reponse)
 # 将html格式转换成一类对象
 print(document)
