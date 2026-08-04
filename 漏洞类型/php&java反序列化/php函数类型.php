e7a65fbf0fde1a1eb5ae3059a135e2c004f3964c
whb54012 <1226844928@qq.com>
2026-08-04 06:18:25 +0000

    自动提交：更新 ${2026/8/4 14:18ilenPM18e}

--- 漏洞类型/php&java反序列化/php函数类型.php
+++ 漏洞类型/php&java反序列化/php函数类型.php
@@ -38,10 +38,11 @@ class person{
 }
 $a = new person('参数');
 echo $a->name;//打印whb
-print_r($a);
+var_dump($a);
 $serialized = serialize($a);//先将对象序列化为字符串
+echo $serialized;
 $b = unserialize($serialized);//打印对象被反序列化了
-print_r($b);
+var_dump($b);
 //php结束,变量销毁,打印对象销毁了
 ?>
 
