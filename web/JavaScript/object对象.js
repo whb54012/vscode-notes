a18e0c4d66cd4db5378c47cffead5854267a23c6
whb54012 <1226844928@qq.com>
2026-08-07 06:55:20 +0000

    自动提交：更新 ${2026/8/7 14:55ilenPM55e}

--- web/JavaScript/object对象.js
+++ web/JavaScript/object对象.js
@@ -11,13 +11,6 @@ let user = {
 console.log(user.name);
 console.log(user['20']);
 
-// 函数调用
-// 1.普通函数直接调用
-function per(name){
-  return "hello"+name;
-}
-var x=per('whb');
-console.log(x);
 //2.构造函数加new
 function Person(name, age) {
   this.name = name;//前一个name是自己设置的属性，也可以设置为其他单词，后一个为接受对象不可修改
