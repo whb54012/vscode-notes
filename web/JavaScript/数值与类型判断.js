c7723c01e4e3a7c2a7b896a750f5ab9b7e945a03
whb54012 <1226844928@qq.com>
2026-08-07 05:44:47 +0000

    自动提交：更新 ${2026/8/7 13:44ilenPM44e}

--- web/JavaScript/数值与类型判断.js
+++
@@ -1,8 +0,0 @@
-var a=1;
-var b=1;
-var c="1";
-var d="2";
-console.log(a==b);
-console.log(a==c);//双等号时比较内容，无需在意类型
-console.log(a===c);//三等号时会比较类型和内容两者
-console.log(a!==c);//不全等或不全等
--- web/JavaScript/数值类型.js
+++ web/JavaScript/数值类型.js
@@ -9,3 +9,9 @@ let arr3=new Array(1,2,3);
 //如let arr1=Array[1]会将它理解为长度为一的数组;
 document.write(arr1);
 //当输出直接写数组本身时，他是直接输出所有元素
+var c="1";
+var d="2";
+console.log(a==b);
+console.log(a==c);//双等号时比较内容，无需在意类型
+console.log(a===c);//三等号时会比较类型和内容两者
+console.log(a!==c);//不等或不全等
