ae75ca38a2c93398ed13ef81dfa6e714aababea0
whb54012 <1226844928@qq.com>
2026-08-11 08:12:50 +0000

    自动提交：更新 ${2026/8/11 16:12ilenPM12e}

--- 漏洞类型/sql注入/基础注入库名.sql
+++ 漏洞类型/sql注入/基础注入库名.sql
@@ -39,6 +39,6 @@ select object_schema,object_name from performance_schema.table_objects
 -- 搭配show查找字段名
 
 -- sys.schema_tables
-table_schema,table_name,column_name,index_name
+table_schema,table_name,index_name
 select * from schema_table_statistics where table_schema=database();
 -- 和information使用类似语法
