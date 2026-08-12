e90eb8a5d28c537966dd57c37cdea37102d66d5b
whb54012 <1226844928@qq.com>
2026-08-12 04:55:13 +0000

    自动提交：更新 ${2026/8/12 12:55ilenPM55e}

--- python/面向对象/多态.py
+++ python/面向对象/多态.py
@@ -2,18 +2,18 @@ class father:
     def __init__(self,name,age):
         self.name=name
         self.age=age
-class pig(father):
+class p(father):
     def se(self):
         print(f"{self.name}为{self.age}岁了")
-class dog(father):
+class d(father):
     def se(self):
         print(f"{self.name}为{self.age}岁了")
-class cat(father):
+class c(father):
     def se(self):
         print(f"{self.name}为{self.age}岁了")
 
 def func(animal):
     animal.se()
-func(pig(pig,1))
-func(dog(dog,2))
-func(cat(cat,3))
+func(p(pig,1))
+func(d(dog,2))
+func(c(cat,3))
