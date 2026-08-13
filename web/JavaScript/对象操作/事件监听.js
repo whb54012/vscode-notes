4cd45fb4513a2838eb0abd06d4322bd4e17730d0
whb54012 <1226844928@qq.com>
2026-08-13 07:08:08 +0000

    自动提交：更新 ${2026/8/13 15:08ilenPM8e}

--- web/JavaScript/对象操作/事件监听.js
+++ web/JavaScript/对象操作/事件监听.js
@@ -2,11 +2,14 @@
 // 元素对象.on+事件=函数
 const btn=document.getElementById("id")
 btn.onclick=function(){}
+// 第一种给同一个元素赋多种监听事件会被覆盖,只保留最后一个
 
 第二种
 // 元素对象.addeventlistener('事件类型',执行函数)
 const btn=document.getElementById("id")
 btn.addEventListener('click',function(){})
+// 第二种同一个元素赋多种监听事件会依次执行,不会覆盖
 
 事件类型:
 点击:click
+MouseEvent
