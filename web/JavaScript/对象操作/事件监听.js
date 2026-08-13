c285374105068134bf8c15e1210b1114ce8f46b7
whb54012 <1226844928@qq.com>
2026-08-13 07:05:37 +0000

    自动提交：更新 ${2026/8/13 15:05ilenPM5e}

--- web/JavaScript/对象操作/事件监听.js
+++ web/JavaScript/对象操作/事件监听.js
@@ -4,5 +4,9 @@ const btn=document.getElementById("id")
 btn.onclick=function(){}
 
 第二种
-元素对象.addeventlistener('事件类型',执行函数)
+// 元素对象.addeventlistener('事件类型',执行函数)
+const btn=document.getElementById("id")
+btn.addEventListener('click',function(){})
+
+事件类型:
 点击:click
