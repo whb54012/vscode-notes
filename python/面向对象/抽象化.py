4a3b5211aa06a6c8dfa485bde09a9fb91ba44209
whb54012 <1226844928@qq.com>
2026-08-13 06:59:28 +0000

    自动提交：更新 ${2026/8/13 14:59ilenPM59e}

--- python/面向对象/抽象化.py
+++ python/面向对象/抽象化.py
@@ -8,7 +8,7 @@ class father(ABC):
 class son:
     def __init__(self):
         pass
-    def must(self):
-        pass
+    def must(self):# 提醒开发者子类要用自己的方法
+        print("子类方法")
 # 抽象化函数用于提醒,被抽象化的父类方法子类必须也有,没有就会报错提醒,用于
 # 子类太多了忘记给子类添加方法的时候
