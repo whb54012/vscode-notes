80f8c9cba1ee90f0c4c9602fb21bf88dccdbf3bf
whb54012 <1226844928@qq.com>
2026-08-15 06:15:38 +0000

    自动提交：更新 ${2026/8/15 14:15ilenPM15e}

--- python/红包6脚本.py
+++ python/红包6脚本.py
@@ -1,5 +1,5 @@
 import requests,threading,hashlib
-minute=input("输入时分")
+minute=hashlib.md5(input("输入时分"))
 url=input("输入网址")
 with open("C:\\Users\\whb\\Downloads\\key (1).dat","rb")as f:
     data1=f.read()
@@ -7,7 +7,7 @@ with open("C:\\Users\\whb\\Downloads\\key (1).dat","rb")as f:
 def func(data):
     reponse=requests.post(url=url,data=data,verify=False)
     with lock:
-        print(reponse.text()+"\n")
+        print(reponse.text+"\n")
 for i in range(1000):
     thred=[]
     thred.append()=threading.Thread(target=func,args=(data1,))
