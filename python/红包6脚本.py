7566d2cd7cc81270fa785400c2d870e96eb83836
whb54012 <1226844928@qq.com>
2026-08-15 06:14:15 +0000

    自动提交：更新 ${2026/8/15 14:14ilenPM14e}

--- python/红包6脚本.py
+++ python/红包6脚本.py
@@ -10,8 +10,8 @@ def func(data):
         print(reponse.text()+"\n")
 for i in range(1000):
     thred=[]
-    therd=threading.Thread(target=func,args=(data1,))
-    therd=threading.Thread(target=func,args=(data2,))
+    thred.append()=threading.Thread(target=func,args=(data1,))
+    thred=threading.Thread(target=func,args=(data2,))
 lock=threading.Lock()
-for i in range(2000): therd[i].start()
+for i in range(2000): thred[i].start()
     
