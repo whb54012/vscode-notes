8114cb21eb839ae002428d5887c941f41ef10ca2
whb54012 <1226844928@qq.com>
2026-08-16 14:37:43 +0000

    自动提交：更新 ${2026/8/16 22:37ilenPM37e}

---
+++ 1.php
@@ -0,0 +1,58 @@
+<?php
+class Info{
+    public $age;
+    public $nickname;
+    public $CtrlCase;
+    public function __construct($age,$nickname){
+        $this->age=$age;
+        $this->nickname=$nickname;
+    }   
+}
+Class UpdateHelper{
+    public $id;
+    public $newinfo;
+    public $sql;
+    public function __construct($newInfo,$sql){
+        $newInfo=unserialize($newInfo);
+        $upDate=new dbCtrl();
+    }
+    public function __destruct()
+    {
+        echo $this->sql;
+    }
+}
+class dbCtrl
+{
+    public $hostname="127.0.0.1";
+    public $dbuser="noob123";
+    public $dbpass="noob123";
+    public $database="noob123";
+    public $name;
+    public $password;
+    public $mysqli;
+    public $token;
+    public function __construct()
+    {
+        $this->name=$_POST['username'];
+        $this->password=$_POST['password'];
+        $this->token=$_SESSION['token'];
+    }
+    public function login($sql)
+    {
+        $this->mysqli=new mysqli($this->hostname, $this->dbuser, $this->dbpass, $this->database);
+        if ($this->mysqli->connect_error) {
++
+            die("连接失败，错误:" . $this->mysqli->connect_error);
+        }
+        $result=$this->mysqli->prepare($sql);
+        $result->bind_param('s', $this->name);
+        $result->execute();
+        $result->bind_result($idResult, $passwordResult);
+        $result->fetch();
+        $result->close();
+        if ($this->token=='admin') {
+            return $idResult;
+        }
+    }
+}
+?>
