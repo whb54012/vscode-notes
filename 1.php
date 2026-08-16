ca9f80950e1e9a42c511b3645f22db7d55194294
whb54012 <1226844928@qq.com>
2026-08-16 14:41:36 +0000

    自动提交：更新 ${2026/8/16 22:41ilenPM41e}

--- 1.php
+++ 1.php
@@ -3,10 +3,10 @@ class Info{
     public $age;
     public $nickname;
     public $CtrlCase;
-    public function __construct($age,$nickname){
-        $this->age=$age;
-        $this->nickname=$nickname;
-    }   
+    public function __construct($age = 0, $nickname = 'guest'){
+        $this->age = $age;
+        $this->nickname = $nickname;
+    }
 }
 Class UpdateHelper{
     public $id;
