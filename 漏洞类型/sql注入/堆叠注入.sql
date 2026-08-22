9a0f82f01c1a923effacb0e4407cc707926c6bf9
whb54012 <1226844928@qq.com>
2026-08-22 15:11:01 +0000

    自动提交：更新 ${2026/8/22 23:11ilenPM11e}

--- 漏洞类型/sql注入/堆叠注入.sql
+++ 漏洞类型/sql注入/堆叠注入.sql
@@ -1,7 +1,5 @@
 select username from user;select password from user;
 通过用分号隔绝语句来堆叠注入多个语句进行注入攻击
-handler 表名 open;handler 表名 first;handler 表名 next;
-handler 表名 close;读取表里的数据,读几行就用next往下面继续
 后端php连接数据库函数要求
 mysqli_query()
 //只支持一条语句
