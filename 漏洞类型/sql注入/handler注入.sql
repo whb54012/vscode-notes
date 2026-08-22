0e52dcdd4265c487c7d2fc5a9e3201e5840c82d1
whb54012 <1226844928@qq.com>
2026-08-22 15:17:47 +0000

    自动提交：更新 ${2026/8/22 23:17ilenPM17e}

--- 漏洞类型/sql注入/handler注入.sql
+++ 漏洞类型/sql注入/handler注入.sql
@@ -1,8 +1,10 @@
 handler 库名.表名 open;(如果没有入库打开表格的时候前面加上库名就行)
 handler 表名 read first;
 将handler指针重置到第一行开始读取
-
+handler 表名 read first limit 行数;
+一次性读取几行
 handler 表名 read next;
-handler指针指向下一行,如果下一行没有数据就返回空
+handler指针指向下一行,且无法回到上一行,如果下一行为空就不会显示
 
 handler 表名 close;
+关闭handler指针
