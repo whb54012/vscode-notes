c4671aa47a24c12033f302885ba006bc74da9890
whb54012 <1226844928@qq.com>
2026-09-12 09:30:53 +0000

    自动提交：更新 ${2026/9/12 17:30ilenPM30e}

--- 漏洞类型/代码审计/返回目录的函数.php
+++ 漏洞类型/代码审计/返回目录的函数.php
@@ -20,6 +20,8 @@ while($f=readdir($变量)){echo $f;}
 
 scandir(".");scandir(chr(46));scandir(current(localeconv()));scandir(getcwd());
 // 接收目录或.来打印当前目录文件，和上述一样,返回数组
-scandir("内容")['数组位次'];
+
+scandir("内容")['下标'];
+glob("内容")['下标'];
 // 直接取出数组元素也可用
 ?>
