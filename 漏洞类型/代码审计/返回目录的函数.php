428f730c66b024ec8d399d36c5950b7b27492958
whb54012 <1226844928@qq.com>
2026-09-12 09:09:05 +0000

    自动提交：更新 ${2026/9/12 17:09ilenPM9e}

--- 漏洞类型/代码审计/返回目录的函数.php
+++ 漏洞类型/代码审计/返回目录的函数.php
@@ -1,17 +1,20 @@
 <?php
-getcwd();
-glob("字符串类型");//*打印所有当前目录所有文件名
-scandir("字符串类型");//.打印当前目录所有文件名
-
+getcwd();//显示当前目录
+glob("字符串类型/可用chr()变化或其他函数组合返回");//*显示所有当前目录所有文件名
+scandir("字符串类型/可用chr()变化或其他函数组合返回");//.显示当前目录所有文件名
 
 print_r(getcwd());
 // 返回文件当前目录
+
 glob("*");glob(chr(42));
 // 正则匹配返回数组,通过通配符来查找当前目录符合条件的文件,可通过../来逃逸
+
 $变量=opendir(".");$变量=opendir(chr(46));$变量=opendir(current(localeconv()));
 //  用变量接收当前目录的所有文件,./可以写成.,../写成..
+
 while($f=readdir($变量)){echo $f;} 
 // 循环接收每行数据并打印
+
 scandir(".");scandir(chr(46));scandir(current(localeconv()));
 // 和上述一样,返回数组
 ?>
