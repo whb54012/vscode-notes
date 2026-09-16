95fae28c3289a5773f9a4fca9c7e55549899bd54
whb54012 <1226844928@qq.com>
2026-09-16 10:49:43 +0000

    自动提交：更新 ${2026/9/16 18:49ilenPM49e}

--- db.php
+++ db.php
@@ -4,8 +4,12 @@ $user='root';
 $pass='root';
 $t_name='user';
 function connect(){
-    $link=mysqli_connect($local,$user,$pass,'null');
-
-
+    global $local,$user,$pass;
+    $link=mysqli_connect($local,$user,$pass);
+    mysqli_query($link,"set name utf8mb4");
+    if(!$link){
+        die("数据库连接失败". mysqli_connect_error());
+    }
+    return $link;
 }
 ?>
