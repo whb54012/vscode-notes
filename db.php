6558512ff82f30e3f14966a5f1cf1e727a178b34
whb54012 <1226844928@qq.com>
2026-09-16 11:54:37 +0000

    自动提交：更新 ${2026/9/16 19:54ilenPM54e}

--- db.php
+++ db.php
@@ -33,7 +33,7 @@ function check(string $username,string $password){
         $end=1;
     }
     if($end==1){
-        die(echo "123");
+        return false;
     }
     $result=mysqli_query($link,$sql);
     return $result;
