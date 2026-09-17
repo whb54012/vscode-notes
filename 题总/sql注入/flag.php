2f2e71d4c2eb93453deb409f5c83ab0b3dd5dd25
whb54012 <1226844928@qq.com>
2026-09-17 10:44:40 +0000

    自动提交：更新 ${2026/9/17 18:44ilenPM44e}

--- 题总/sql注入/flag.php
+++ 题总/sql注入/flag.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION['id'])||$_SESSION['id']!==1){
 <body>
   <div class="box">
     <h1>管理后台</h1>
-    <p>CquetCTF{sheloveme}</p>
+    <p>CquetCTF{iloveshe}</p>
   </div>
 </body>
 </html>
